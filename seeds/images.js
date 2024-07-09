a6a0f413afae3547e38adde09b9f36785adc3f20
DaBuggins <willenrowe@gmail.com>
2024-07-09 20:29:47 +0100

    deploy commit

---
+++ seeds/images.js
@@ -0,0 +1,38 @@
+module.exports.url = [
+    'https://res.cloudinary.com/deskiol0z/image/upload/v1679138839/BugginOut/x0g8avsptzrmlxiv8jrf.jpg',
+    'https://res.cloudinary.com/deskiol0z/image/upload/v1679138840/BugginOut/qyy6hgozxort0dcrgnsg.jpg',
+    'https://res.cloudinary.com/deskiol0z/image/upload/v1679138840/BugginOut/ivvt9fjpk5r2yaqp2fn9.jpg',
+    'https://res.cloudinary.com/deskiol0z/image/upload/v1679138839/BugginOut/wwfifmewbvg4wnaygogk.jpg',
+    'https://res.cloudinary.com/deskiol0z/image/upload/v1681035322/BugginOut/pzvh3uh2vtymurbz432u.png',
+    'https://res.cloudinary.com/deskiol0z/image/upload/v1684950309/BugginOut/rosie-kerr-0VEpqskYPgI-unsplash_wunph0.jpg',
+    'https://res.cloudinary.com/deskiol0z/image/upload/v1684950309/BugginOut/krzysztof-niewolny-T3EzlprIYDk-unsplash_ckzpqw.jpg',
+    'https://res.cloudinary.com/deskiol0z/image/upload/v1684950309/BugginOut/umesh-soni-jcfV6qbbYnk-unsplash_dh7ckx.jpg',
+    'https://res.cloudinary.com/deskiol0z/image/upload/v1684950310/BugginOut/dmitry-bukhantsov-5R_Rz5a60RE-unsplash_covymu.jpg',
+    'https://res.cloudinary.com/deskiol0z/image/upload/v1684950311/BugginOut/james-wainscoat-GGewLGcQD-I-unsplash_bxaflo.jpg',
+    'https://res.cloudinary.com/deskiol0z/image/upload/v1684950313/BugginOut/mikhail-vasilyev-Vf1JrKMUS0Q-unsplash_p238hr.jpg',
+    'https://res.cloudinary.com/deskiol0z/image/upload/v1684950313/BugginOut/bankim-desai--e7iZUbnu5U-unsplash_r9veny.jpg',
+
+
+    
+
+
+
+  ];
+
+
+//   {
+//     url: 'https://res.cloudinary.com/deskiol0z/image/upload/v1679138839/BugginOut/x0g8avsptzrmlxiv8jrf.jpg',
+//     filename: 'BugginOut/x0g8avsptzrmlxiv8jrf',
+//   },
+//   {
+//     url: 'https://res.cloudinary.com/deskiol0z/image/upload/v1679138840/BugginOut/qyy6hgozxort0dcrgnsg.jpg',
+//     filename: 'BugginOut/qyy6hgozxort0dcrgnsg',
+//   },
+//   {
+//     url: 'https://res.cloudinary.com/deskiol0z/image/upload/v1679138840/BugginOut/rxx0hopbzpn0nacmft6y.jpg',
+//     filename: 'BugginOut/rxx0hopbzpn0nacmft6y',
+//   },
+//   {
+//     url: 'https://res.cloudinary.com/deskiol0z/image/upload/v1679138840/BugginOut/ivvt9fjpk5r2yaqp2fn9.jpg',
+//     filename: 'BugginOut/ivvt9fjpk5r2yaqp2fn9',
+//   },
